2052867132e53f3ba04ff1f6a44468630d825182
Vincent Lejeune <vincentcmlejeune@gmail.com>
2026-06-28 23:19:18 +0200

    fix router attempt

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,10 @@
 import { HealthCheck } from "@/components/HealthCheck";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (<><div>Fuck you</div>
         <HealthCheck />
         <p>Né à une époque où Internet était un refuge.</p>
+        <Link to="/sandbox">Sandbox</Link>
     </>)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from "path";
 
 export default defineConfig({
-  base: '/sindaril/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
