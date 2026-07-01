2f2569af6793e07fe6b850da219072b086302d32
Vincent Lejeune <vincentcmlejeune@gmail.com>
2026-07-01 10:25:15 +0200

    feedback component and route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/home/Home'
+import Feedback from '../pages/feedback/Feedback'
 import Menu from '../pages/menu/Menu'
 import Sandbox from '../pages/sandbox/Sandbox'
 
@@ -8,10 +9,14 @@ export const router = createBrowserRouter([
         path: '/', element: <Home />
     },
     {
+        path: '/feedback', element: <Feedback />
+    },
+    {
         path: '/menu', element: <Menu />
     },
     {
         path: '/sandbox', element: <Sandbox />
     }
+
 ],
     { basename: '/sindaril/' });
