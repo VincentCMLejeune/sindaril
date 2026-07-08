322a0794b04a82e5360676ee0ef9281a53055697
Vincent Lejeune <vincentcmlejeune@gmail.com>
2026-07-08 21:08:29 +0200

    no u

--- src/pages/menu/Menu.tsx
+++ src/pages/menu/Menu.tsx
@@ -6,6 +6,7 @@ export default function Menu() {
     return (<><div>Fuck you</div>
         <HealthCheck />
         <p>Né à une époque où Internet était un refuge.</p>
+        <p>Repars, imbécile, Internet est mort.</p>
         <Link to="/sandbox">Sandbox</Link>
         <Link to="/feedback">Feedback</Link>
     </>)
